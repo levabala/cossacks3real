465e71f8482373a1f1112818a12904b014660d1f
levabala <levabalaanother@gmail.com>
2023-10-06 12:12:05 +0200

    feat(unit): very basic movement + render as nice blue square

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use bevy::prelude::*;
 mod unit;
+mod unit2d;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, unit::UnitPlugin))
+        .add_plugins((DefaultPlugins, unit::UnitPlugin, unit2d::Unit2dPlugin))
         .run()
 }
--- src/unit.rs
+++ src/unit.rs
@@ -1,33 +1,51 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Unit;
+pub struct Unit;
 
 #[derive(Component)]
-struct Position {
-    x: f32,
-    y: f32,
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
 }
 
-fn add_units(mut commands: Commands) {
-    commands.spawn((Unit, Position { x: 1.0, y: 1.0 }));
-    commands.spawn((Unit, Position { x: 2.0, y: 1.0 }));
+#[derive(Component)]
+pub enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+    None,
+}
+
+fn unit_add(mut commands: Commands) {
+    let units_array = [
+        (Unit, Position { x: 0.0, y: 0.0 }, Direction::Up),
+        (Unit, Position { x: 10.0, y: 1.0 }, Direction::Right),
+        (Unit, Position { x: 20.0, y: 30.0 }, Direction::Right),
+        (Unit, Position { x: 30.0, y: 20.0 }, Direction::None),
+    ];
+
+    for unit in units_array {
+        commands.spawn(unit);
+    }
 }
 
 #[derive(Resource)]
 struct PrintTimer(Timer);
 
-fn print_position_system(
-    time: Res<Time>,
-    mut timer: ResMut<PrintTimer>,
-    query: Query<&Position, With<Unit>>,
-) {
-    if !timer.0.tick(time.delta()).just_finished() {
-        return;
-    }
+fn unit_move(time: Res<Time>, mut query: Query<(&mut Position, &Direction), With<Unit>>) {
+    const SPEED: f32 = 30.;
 
-    for position in &query {
-        println!("position: {} {}", position.x, position.y)
+    for (mut position, direction) in &mut query {
+        let delta = SPEED * time.delta_seconds();
+        match direction {
+            Direction::Up => position.y += delta,
+            Direction::Down => position.y -= delta,
+            Direction::Right => position.x += delta,
+            Direction::Left => position.x -= delta,
+            Direction::None => (),
+        }
     }
 }
 
@@ -36,7 +54,7 @@ pub struct UnitPlugin;
 impl Plugin for UnitPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(PrintTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_units)
-            .add_systems(Update, print_position_system);
+            .add_systems(Startup, unit_add)
+            .add_systems(Update, unit_move);
     }
 }

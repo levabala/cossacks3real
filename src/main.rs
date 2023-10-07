387dae52b1a7a3fab86673f0647e5819cab4a203
levabala <levabalaanother@gmail.com>
2023-10-07 22:00:20 +0200

    feat: unit waypoint render

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use bevy::prelude::*;
 mod unit;
 mod unit_renderer;
+mod unit_waypoint_renderer;
 
 fn main() {
     App::new()
+        .add_systems(Startup, || {
+            println!("start\n");
+        })
         .add_plugins((
             DefaultPlugins,
             unit::UnitPlugin,
             unit_renderer::UnitRendererPlugin,
+            unit_waypoint_renderer::UnitWaypointRendererPlugin,
         ))
         .run()
 }

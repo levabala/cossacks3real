ac80f83f57a4ae499f315bb9b7ea331c479d06ac
levabala <levabalaanother@gmail.com>
2023-10-08 23:23:07 +0200

    feat(unit_controls): create waypoint after select + right click

--- src/unit_controls_mouse.rs
+++ src/unit_controls_mouse.rs
@@ -1,8 +1,14 @@
+use crate::map::*;
 use crate::unit::*;
+use crate::unit_move::*;
 use bevy::prelude::*;
 use bevy_mod_picking::prelude::*;
+use std::collections::VecDeque;
 
-fn update_unit_drawing(mut commands: Commands, query: Query<Entity, Added<Unit>>) {
+#[derive(Component)]
+pub struct Initialized;
+
+fn make_unit_pickable(mut commands: Commands, query: Query<Entity, Added<Unit>>) {
     for entity in query.iter() {
         commands
             .entity(entity)
@@ -10,10 +16,61 @@ fn update_unit_drawing(mut commands: Commands, query: Query<Entity, Added<Unit>>
     }
 }
 
+const WAYPOINT_Z_OFFSET: f32 = 10.;
+
+fn create_waypoints_for_selected(
+    event: Listener<Pointer<Click>>,
+    mut commands: Commands,
+    mut query: Query<(Entity, &PickSelection, Option<&mut Waypoints>), With<Unit>>,
+) {
+    if event.button != PointerButton::Secondary {
+        return;
+    }
+
+    let Some(position) = event.hit.position else {
+        println!("no position is presented");
+        return;
+    };
+
+    for (entity, pick_selection, waypoints) in &mut query {
+        if !pick_selection.is_selected {
+            continue;
+        }
+
+        let waypoint_position = Vec3 {
+            z: position.z + WAYPOINT_Z_OFFSET,
+            ..position
+        };
+
+        match waypoints {
+            Some(mut w) => w.0.push_back(waypoint_position),
+            None => {
+                commands
+                    .entity(entity)
+                    .insert(Waypoints(VecDeque::from([waypoint_position])));
+            }
+        }
+    }
+}
+
+fn add_map_click_listener(
+    mut commands: Commands,
+    query: Query<Entity, (With<Map>, Without<Initialized>)>,
+) {
+    for entity in query.iter() {
+        commands
+            .entity(entity)
+            .insert((PickableBundle::default(), RaycastPickTarget::default()))
+            .remove::<(PickSelection, PickHighlight)>()
+            .insert(On::<Pointer<Click>>::run(create_waypoints_for_selected))
+            .insert(Initialized);
+    }
+}
+
 pub struct UnitControlsMouse;
 
 impl Plugin for UnitControlsMouse {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, update_unit_drawing);
+        app.add_systems(Update, (make_unit_pickable, add_map_click_listener));
     }
 }

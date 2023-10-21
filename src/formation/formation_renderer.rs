2f5bd305429dffd590c8c0cf3889ba73b449b54a
levabala <levabalaanother@gmail.com>
2023-10-21 18:44:46 +0200

    feat(formation): update outline when moving

--- src/formation/formation_renderer.rs
+++ src/formation/formation_renderer.rs
@@ -24,19 +24,53 @@ fn generate_zone_vertices(zone: &Zone) -> Vec<Vec3> {
     return Vec::from(verticies);
 }
 
-fn set_transform_formation(
+fn calc_formation_translation(zone: &Zone) -> Transform {
+    let mut transform = Transform::from_translation(zone.position);
+    transform.rotate_z(-zone.direction.angle_between(Vec2::Y));
+
+    return transform;
+}
+
+fn setup_transform_formation(
     mut commands: Commands,
     query: Query<(Entity, &Zone), (With<Formation>, Added<Zone>)>,
 ) {
     for (entity, zone) in query.iter() {
-        let mut transform = Transform::from_translation(zone.position);
-        transform.rotate_z(-zone.direction.angle_between(Vec2::Y));
-
         commands.entity(entity).insert(PbrBundle {
-            transform,
+            transform: calc_formation_translation(zone),
             ..default()
         });
     }
+}
+
+fn update_transform_formation(
+    mut commands: Commands,
+    query: Query<(Entity, &Zone), (With<Formation>, Changed<Zone>)>,
+) {
+    for (entity, zone) in query.iter() {
+        commands
+            .entity(entity)
+            .insert(calc_formation_translation(zone));
+    }
+}
+
+fn create_formation_outline(
+    polyline_materials: &mut ResMut<Assets<PolylineMaterial>>,
+    polylines: &mut ResMut<Assets<Polyline>>,
+    zone: &Zone,
+) -> PolylineBundle {
+    return PolylineBundle {
+        polyline: polylines.add(Polyline {
+            vertices: generate_zone_vertices(zone),
+        }),
+        material: polyline_materials.add(PolylineMaterial {
+            width: 1.0,
+            color: Color::RED,
+            perspective: false,
+            ..default()
+        }),
+        ..default()
+    };
 }
 
 // TODO: use regular mesh?
@@ -48,21 +82,38 @@ fn draw_formation_outline(
 ) {
     for (entity, zone) in query.iter() {
         let outline = commands
-            .spawn(PolylineBundle {
-                polyline: polylines.add(Polyline {
-                    vertices: generate_zone_vertices(zone),
-                }),
-                material: polyline_materials.add(PolylineMaterial {
-                    width: 1.0,
-                    color: Color::RED,
-                    perspective: false,
-                    ..default()
-                }),
-                ..default()
-            })
+            .spawn(create_formation_outline(
+                &mut polyline_materials,
+                &mut polylines,
+                zone,
+            ))
             .id();
 
         commands.entity(entity).add_child(outline);
+    }
+}
+
+// TODO: modify instead of despawn-respawn
+fn update_formation_outline(
+    mut commands: Commands,
+    mut polyline_materials: ResMut<Assets<PolylineMaterial>>,
+    mut polylines: ResMut<Assets<Polyline>>,
+    query: Query<(Entity, &Zone, &Children), (With<Formation>, Changed<Zone>)>,
+    query_outline: Query<Entity, With<Handle<Polyline>>>,
+) {
+    for (entity, zone, children) in query.iter() {
+        for outline_outdated in query_outline.iter_many(children) {
+            commands.entity(outline_outdated).despawn();
+
+            let outline_new = commands
+                .spawn(create_formation_outline(
+                    &mut polyline_materials,
+                    &mut polylines,
+                    zone,
+                ))
+                .id();
+            commands.entity(entity).add_child(outline_new);
+        }
     }
 }
 
@@ -129,9 +180,11 @@ impl Plugin for FormationRendererPlugin {
             .add_systems(
                 Update,
                 (
-                    set_transform_formation,
+                    setup_transform_formation,
+                    update_transform_formation,
                     draw_formation_outline,
                     draw_formation_slots,
+                    update_formation_outline,
                 ),
             )
             .add_systems(PostUpdate, handle_formation_slots_removal);

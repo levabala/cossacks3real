88d8188ddd26586775facf409738ba9c77fb2892
levabala <levabalaanother@gmail.com>
2023-10-23 21:16:29 +0200

    wip

--- src/unit/unit_waypoint_renderer.rs
+++ src/unit/unit_waypoint_renderer.rs
@@ -3,19 +3,24 @@ use crate::unit_move::*;
 use bevy::prelude::*;
 use std::collections::VecDeque;
 
+struct WaypointDrawed {
+    position: Vec3,
+    entity: Entity,
+}
+
 #[derive(Component)]
-struct WaypointsDrawed(VecDeque<Vec3>);
+struct WaypointDrawedList(VecDeque<WaypointDrawed>);
 
 const SIZE: f32 = 1.0;
 
 fn create_waypoints_drawed(
     mut commands: Commands,
-    mut query: Query<Entity, (With<Unit>, Without<WaypointsDrawed>)>,
+    mut query: Query<Entity, (With<Unit>, Without<WaypointDrawedList>)>,
 ) {
     for entity in &mut query {
         commands
             .entity(entity)
-            .insert(WaypointsDrawed(VecDeque::new()));
+            .insert(WaypointDrawedList(VecDeque::new()));
     }
 }
 
@@ -23,30 +28,49 @@ fn draw_waypoints(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut query: Query<(&Waypoints, &mut WaypointsDrawed), With<Unit>>,
+    mut query: Query<(&Waypoints, &mut WaypointDrawedList), (With<Unit>, Changed<Waypoints>)>,
 ) {
-    for (waypoints, mut waypoints_drawed) in &mut query {
-        for waypoint in waypoints.0.iter() {
-            match waypoints_drawed.0.iter().position(|w| w == waypoint) {
-                Some(_) => continue,
-                None => waypoints_drawed.0.push_back(*waypoint),
+    for (waypoint_list, mut waypoint_drawed_list) in &mut query {
+        // TODO: compare only the first and the last one
+        for waypoint_drawed in waypoint_drawed_list.0.iter() {
+            let is_waypoint_deleted = !waypoint_list.0.contains(&waypoint_drawed.position);
+            if is_waypoint_deleted {
+                commands.entity(waypoint_drawed.entity).despawn();
+                waypoint_drawed_list.0.pop_back();
             }
+        }
 
-            commands.spawn(PbrBundle {
-                mesh: meshes.add(
-                    shape::UVSphere {
-                        radius: SIZE,
+        for waypoint in waypoint_list.0.iter() {
+            let is_waypoint_added = waypoint_drawed_list
+                .0
+                .iter()
+                .position(|w| &w.position == waypoint)
+                .is_none();
+
+            if is_waypoint_added {
+                let waypoint_drawing = commands
+                    .spawn(PbrBundle {
+                        mesh: meshes.add(
+                            shape::UVSphere {
+                                radius: SIZE,
+                                ..default()
+                            }
+                            .into(),
+                        ),
+                        material: materials.add(StandardMaterial {
+                            base_color: Color::RED,
+                            ..default()
+                        }),
+                        transform: Transform::from_translation(*waypoint),
                         ..default()
-                    }
-                    .into(),
-                ),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::RED,
-                    ..default()
-                }),
-                transform: Transform::from_translation(*waypoint),
-                ..default()
-            });
+                    })
+                    .id();
+
+                waypoint_drawed_list.0.push_back(WaypointDrawed {
+                    position: *waypoint,
+                    entity: waypoint_drawing,
+                });
+            }
         }
     }
 }
@@ -55,6 +79,12 @@ pub struct UnitWaypointRendererPlugin;
 
 impl Plugin for UnitWaypointRendererPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (create_waypoints_drawed, draw_waypoints));
+        app.add_systems(
+            Update,
+            (
+                create_waypoints_drawed,
+                draw_waypoints.after(create_waypoints_drawed),
+            ),
+        );
     }
 }

6db859f39793a3b859e3c65e60095ec582a2dfc3
Lev <levabalaanother@gmail.com>
2023-10-24 22:58:41 +0200

    26 remove old waypoints (#28)
    * refactor(unit_waypoint/render): remove .clone()
    
    * wip
    
    * feat(unit_waypoint/render): remove passed waypoints

--- src/unit/unit_waypoint_renderer.rs
+++ src/unit/unit_waypoint_renderer.rs
@@ -3,19 +3,25 @@ use crate::unit_move::*;
 use bevy::prelude::*;
 use std::collections::VecDeque;
 
-#[derive(Component)]
-struct WaypointsDrawed(VecDeque<Vec3>);
+#[derive(Clone, Copy, Debug)]
+struct WaypointDrawed {
+    position: Vec3,
+    entity: Entity,
+}
+
+#[derive(Component, Debug)]
+struct WaypointDrawedList(VecDeque<WaypointDrawed>);
 
 const SIZE: f32 = 1.0;
 
 fn create_waypoints_drawed(
     mut commands: Commands,
-    mut query: Query<Entity, (With<Unit>, Without<WaypointsDrawed>)>,
+    mut query: Query<Entity, (With<Unit>, Without<WaypointDrawedList>)>,
 ) {
     for entity in &mut query {
         commands
             .entity(entity)
-            .insert(WaypointsDrawed(VecDeque::new()));
+            .insert(WaypointDrawedList(VecDeque::new()));
     }
 }
 
@@ -23,30 +29,54 @@ fn draw_waypoints(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut query: Query<(&Waypoints, &mut WaypointsDrawed), With<Unit>>,
+    mut query: Query<(&Waypoints, &mut WaypointDrawedList), (With<Unit>, Changed<Waypoints>)>,
 ) {
-    for (waypoints, mut waypoints_drawed) in &mut query {
-        // TODO: remove .cloned()
-        for waypoint in waypoints.0.iter().cloned() {
-            match waypoints_drawed.0.iter().position(|w| *w == waypoint) {
-                Some(_) => continue,
-                None => waypoints_drawed.0.push_back(waypoint),
+    for (waypoint_list, mut waypoint_drawed_list) in &mut query {
+        if let Some(waypoint_first) = waypoint_list.0.get(0) {
+            let mut pop_until_index_option: Option<usize> = None;
+            for (index, waypoint_drawed) in waypoint_drawed_list.0.iter().enumerate() {
+                if &waypoint_drawed.position == waypoint_first {
+                    break;
+                }
+
+                commands.entity(waypoint_drawed.entity).despawn();
+                pop_until_index_option = Some(index);
             }
 
-            commands.spawn(PbrBundle {
-                mesh: meshes.add(
-                    shape::UVSphere {
-                        radius: SIZE,
+            if let Some(pop_until_index) = pop_until_index_option {
+                waypoint_drawed_list.0.drain(0..=pop_until_index);
+            }
+        }
+
+        let waypoint_drawed_last_option = waypoint_drawed_list.0.back().map(|r| *r);
+        for waypoint in waypoint_list.0.iter().rev() {
+            if let Some(waypoint_drawed_last) = waypoint_drawed_last_option {
+                if *waypoint == waypoint_drawed_last.position {
+                    break;
+                }
+            }
+
+            let waypoint_drawing = commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(
+                        shape::UVSphere {
+                            radius: SIZE,
+                            ..default()
+                        }
+                        .into(),
+                    ),
+                    material: materials.add(StandardMaterial {
+                        base_color: Color::RED,
                         ..default()
-                    }
-                    .into(),
-                ),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::RED,
+                    }),
+                    transform: Transform::from_translation(*waypoint),
                     ..default()
-                }),
-                transform: Transform::from_translation(waypoint),
-                ..default()
+                })
+                .id();
+
+            waypoint_drawed_list.0.push_back(WaypointDrawed {
+                position: *waypoint,
+                entity: waypoint_drawing,
             });
         }
     }
@@ -56,6 +86,12 @@ pub struct UnitWaypointRendererPlugin;
 
 impl Plugin for UnitWaypointRendererPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (create_waypoints_drawed, draw_waypoints));
+        app.add_systems(
+            Update,
+            (
+                create_waypoints_drawed,
+                draw_waypoints.after(create_waypoints_drawed),
+            ),
+        );
     }
 }
